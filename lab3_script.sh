9039518f7d5031fae8c0954854c03940cecccd0d
Giovanni Visco <gvgio1234@gmail.com>
2019-09-22 12:34:11 -0600

    Finished lab3_script and email_results

---
+++ lab3_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Authors : Giovanni Visco
+# Date: 9/20/2019
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+
+#1
+#prompt user to enter filename and regex
+
+echo "Enter a file name: "
+read fileName
+echo "Enter a regular expression: "
+read regex
+#this will be for the count of emails
+grep -c $regex $fileName
+echo "Enter a new regular expression: "
+read regex2
+#this will be for the 303 area code
+grep -o $regex2 $fileName
+#this will write the emails to the file
+grep "@geocities.com" $fileName >> email_results.txt
--- practice.sh
+++ practice.sh
@@ -13,3 +13,4 @@ echo "The product is: $prod"
 echo "File Name: $0"
 echo "Command Line Argument 1: $1"
 echo "Command Line Argument 2: $2"
+grep $1 $2
